9006d27d0f0bf0147901748b0dd44632e2265f68
Nolager <andresosoriolopez@gmail.com>
2022-08-11 11:11:13 -0400

    Adding new Find Intersection challenge

---
+++ src/main/java/cl/andres/challenges/FindIntersection.java
@@ -0,0 +1,53 @@
+package cl.andres.challenges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Find Intersection
+ *
+ * Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain
+ * 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second
+ * element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a
+ * comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no
+ * intersection, return the string false.
+ *
+ * Examples:
+ *
+ * Input: new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"}
+ * Output: 1,4,13
+ *
+ * Input: new String[] {"1, 3, 9, 10, 17, 18", "1, 4, 9, 10"}
+ * Output: 1,9,10
+ *
+ */
+public class FindIntersection {
+    public static void main(String[] args) {
+        String[] stringArray = new String[] {"1, 3, 4, 7, 13", "1, 2, 4, 13, 15"};
+        String[] stringArray2 = new String[] {"1, 3, 9, 10, 17, 18", "1, 4, 9, 10"};
+
+        System.out.println("Output: " + findIntersection(stringArray));
+        System.out.println("Output: " + findIntersection(stringArray2));
+    }
+
+    public static String findIntersection(String[] strArr) {
+        // code goes here
+        Map<String, String> map = new HashMap<>();
+        List<String> output = new ArrayList<>();
+
+        Arrays.stream(strArr[0].split(",")).forEach(number -> map.put(number, number));
+        Arrays.stream(strArr[1].split(","))
+                .forEach(number -> {
+
+                    if (map.get(number) != null) {
+                        output.add(number);
+                    }
+                });
+
+        return String.join(",", output);
+    }
+}
