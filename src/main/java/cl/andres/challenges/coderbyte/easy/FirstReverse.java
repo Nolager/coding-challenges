8f73bbca4d449cfda17f634d06429f018724a978
Nolager <andresosoriolopez@gmail.com>
2022-08-16 12:39:43 -0400

    Adding First Reverse challenge

---
+++ src/main/java/cl/andres/challenges/coderbyte/easy/FirstReverse.java
@@ -0,0 +1,34 @@
+package cl.andres.challenges.coderbyte.easy;
+
+/**
+ * First Reverse
+ *
+ * Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+ * For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna
+ * dlroW olleH.
+ *
+ * Examples:
+ *
+ * Input: "coderbyte"
+ * Output: etybredoc
+ *
+ * Input: "I Love Code"
+ * Output: edoC evoL I
+ *
+ */
+public class FirstReverse {
+    public static void main(String[] args) {
+        System.out.println(firstReverse("I Love Code"));
+    }
+
+    public static String firstReverse(String str) {
+        // code goes here
+        StringBuilder reverseString = new StringBuilder();
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverseString.append(str.charAt(i));
+        }
+
+        return reverseString.toString();
+    }
+}
