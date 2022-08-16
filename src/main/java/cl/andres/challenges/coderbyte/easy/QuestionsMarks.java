51f5a9c6170a6571c7027af3e804dbd9c7a12765
Nolager <andresosoriolopez@gmail.com>
2022-08-16 12:27:10 -0400

    Adding another solution to Question Mark challenge

--- src/main/java/cl/andres/challenges/coderbyte/easy/QuestionsMarks.java
+++ src/main/java/cl/andres/challenges/coderbyte/easy/QuestionsMarks.java
@@ -76,4 +76,30 @@ public class QuestionsMarks {
 
         return returnValue;
     }
+
+    /**
+     * Solution from https://coderbyte.com/results/burakdede:Questions%20Marks:Java
+     */
+    public static String otherSolution(String str) {
+
+        int i = 0;
+        while(i < str.length()) {
+            if(Character.isDigit(str.charAt(i))) {
+                int j = i + 1;
+                int qCount = 0;
+                while(j < str.length()) {
+                    if(str.charAt(j) == '?') qCount++;
+                    if(Character.isDigit(str.charAt(j))) {
+                        int sum = Integer.parseInt(str.charAt(i) + "") + Integer.parseInt(str.charAt(j) + "");
+                        if(!(sum == 10 && qCount == 3)) return "false";
+                        i = j;
+                        break;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+        return "true";
+    }
 }
